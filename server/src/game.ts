28ca7470fcdaf9059b172e9252a932e81bf8294a
Eugene <oleynik.zh10@gmail.com>
2021-02-23 18:12:20 +0200

    Board state removed and checkWin() works with data form client

--- server/src/game.ts
+++ server/src/game.ts
@@ -1,28 +1,18 @@
 import { Board } from "./types/board"
 import { PlayerSymbol, Turn, WinningCombitation } from "./types/in_game"
 
-let board: Board = [
-    'None', 'None', 'None',
-    'None', 'None', 'None',
-    'None', 'None', 'None'
-]
-
 // [0][1][2]
 // [3][4][5]
 // [6][7][8]
 
-let winningCombitation: Array<WinningCombitation> = [
+const winningCombitation: Array<WinningCombitation> = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ]
 
-export default function turn(data: Turn): WinningCombitation | null {
-    board[data.position] = data.symbol
-    return checkWin(data.symbol)
-}
-
-function checkWin(playerSymbol: PlayerSymbol): WinningCombitation | null {
+export default function checkWin(playerSymbol: PlayerSymbol,
+    board: Board): WinningCombitation | null {
 
     for (let i: number = 0; i < winningCombitation.length; ++i) {
         const set = winningCombitation[i]
